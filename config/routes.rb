52912d0551b5414be4ed674b6301b3f0ffd25e62
figoxu <xujh945@qq.com>
2018-06-04 21:39:37 +0800

    rails g controller login new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'login/new'
+  root :to => 'login#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
